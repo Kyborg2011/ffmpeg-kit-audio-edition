92c5ef03b3f7a8b3c376e0382e48e5dc684b6d30
Anton Babinin <wkyborgw@gmail.com>
2024-04-29 00:12:57 +0300

    fixes

--- scripts/android/libsndfile.sh
+++ scripts/android/libsndfile.sh
@@ -4,10 +4,13 @@
 make distclean 2>/dev/null 1>/dev/null
 
 # REGENERATE BUILD FILES IF NECESSARY OR REQUESTED
-if [[ ! -f "${BASEDIR}"/src/"${LIB_NAME}"/configure ]] || [[ ${RECONF_libsndfile} -eq 1 ]]; then
-  autoreconf_library "${LIB_NAME}" 1>>"${BASEDIR}"/build.log 2>&1 || return 1
-fi
+#if [[ ! -f "${BASEDIR}"/src/"${LIB_NAME}"/configure ]] || [[ ${RECONF_libsndfile} -eq 1 ]]; then
+  #autoreconf_library "${LIB_NAME}" 1>>"${BASEDIR}"/build.log 2>&1 || return 1
+  #export ANDROID_TOOLCHAIN_HOME="${ANDROID_NDK_ROOT}"/toolchains/llvm/prebuilt/"${TOOLCHAIN}"
+  autoreconf -vif 1>>"${BASEDIR}"/build.log 2>&1 || return 1
+#fi
 
+#./Scripts/android-configure.sh
 ./configure \
   --prefix="${LIB_INSTALL_PREFIX}" \
   --with-pic \
